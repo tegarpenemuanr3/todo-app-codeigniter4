f8936ccf09f32153b404e49015a1d0322d4cbc52
tegarpenemuanr3 <tegarpenemuan@gmail.com>
2023-10-22 22:07:25 +0700

    Using view layout (#8)

--- app/Views/todos/index.php
+++ app/Views/todos/index.php
@@ -1,33 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php $this->extend('layout/template'); ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>To-Do App</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap/dist/css/bootstrap.min.css">
-</head>
-
-<body>
-    <div class="container mt-5">
-        <div class="row">
-            <div class="col-md-12">
-                <h1>List Todo</h1>
-                <ul class="list-group">
-                    <?php foreach ($todos as $todo): ?>
-                    <li class="list-group-item d-flex justify-content-between align-items-center">
-                        <div class="form-check">
-                            <input class="form-check-input" type="checkbox" id="todo1">
-                            <label class="form-check-label" for="todo1">
-                                <?= $todo->title ?>
-                            </label>
-                        </div>
-                    </li>
-                    <?php endforeach; ?>
-                </ul>
-            </div>
-        </div>
+<?php $this->section('content'); ?>
+<div class="row">
+    <div class="col-md-12">
+        <h1>List Todo</h1>
+        <ul class="list-group">
+            <?php foreach ($todos as $todo): ?>
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="todo1">
+                    <label class="form-check-label" for="todo1">
+                        <?= $todo->title ?>
+                    </label>
+                </div>
+            </li>
+            <?php endforeach; ?>
+        </ul>
     </div>
-</body>
-
-</html>
+</div>
+<?php $this->endSection(); ?>
