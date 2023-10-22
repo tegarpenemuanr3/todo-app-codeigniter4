d5db6b133703a588aeb2af22ac591994eba82cb6
tegarpenemuanr3 <tegarpenemuan@gmail.com>
2023-10-22 14:23:19 +0700

    Create model (#5)

---
+++ app/Models/TodosModel.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class TodosModel extends Model
+{
+    protected $DBGroup          = 'default'; 
+    protected $table            = 'todos'; // *
+    protected $primaryKey       = 'id'; // *
+    protected $useAutoIncrement = true; 
+    protected $returnType       = 'object'; // array / object  // * 
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = [ // *
+        'title',
+        'desc',
+        'status',
+    ];
+
+    // Dates
+    protected $useTimestamps = false;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+}
+
+// Dibuat dengan:
+// php spark make:model TodosModel
